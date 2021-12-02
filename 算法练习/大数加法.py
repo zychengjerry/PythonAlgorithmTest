11d53b7eb365f5014ea470e8e8ccf5900604b108
Jerry Cheng <zychengjerry@gmail.com>
2021-12-02 10:36:16 +0800

    Update B2

--- 算法练习/大数加法.py
+++ 算法练习/大数加法.py
@@ -1,10 +1,7 @@
-from _typeshed import Self
-
-
 class Solution:
     def add(self, num1, num2):
         return int(num1) + int(num2)
 
     a = int(input('a = '))
     b = int(input('b = '))
-    print(add(Self,a,b))
+    print(add(a,b))
