de6b3a2aee60705ae07d7a3ad74410f71cd81c34
Jerry Cheng <zychengjerry@gmail.com>
2021-12-02 14:59:08 +0800

    update

--- 算法练习/大数加法.py
+++ 算法练习/大数加法.py
@@ -1,7 +1,7 @@
-class Solution:
-    def add(self, num1, num2):
-        return int(num1) + int(num2)
+def add(num1, num2):
+    return int(num1) + int(num2)
 
+if __name__ == '__main__':
     a = int(input('a = '))
     b = int(input('b = '))
     print(add(a,b))
